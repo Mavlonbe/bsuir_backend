44e34738f1ba8c07baedd2240424ec03380cf363
V-outsider <voutsider@tutanota.com>
2023-12-15 05:18:03 +0300

    extended agents

---
+++ agents/kosher_recipes_agent.py
@@ -0,0 +1,37 @@
+from py2neo import Graph
+
+from typing import List
+
+from api.serializers import EventSerializer
+
+class Agent(object):
+    client = Graph("bolt://localhost:7687", auth=("neo4j", "cnbem5ug"))
+    
+    def search_recipes(self, ingredients: EventSerializer) -> dict:
+        recipes = self.client.run(
+            '''
+            MATCH (r:Recipe)-[:CONTAINS_NER]->(n:NER)
+            WHERE n.name IN $ner_list
+            AND NOT EXISTS {
+                MATCH (r)-[:CONTAINS_NER]->(excluded:NER)
+                WHERE excluded.name IN ["pork", "crab", "lobster", "oyster", "mussels", "squid", "octopus", "shark", "eel", "frog", "turtle"]
+                }
+            WITH r, COUNT(n) AS nerCount
+            RETURN r
+            ORDER BY nerCount DESC
+            LIMIT 3
+            ''', {'ner_list': ingredients.data['Message']}
+            )
+        
+        return recipes.to_data_frame().to_dict()
+    
+    @classmethod
+    def trigger(cls, event: EventSerializer) -> (dict, None):
+        if event.data['EventType'] == "Search_Recipe_By_Ingredients_Jevish":
+            return cls.search_recipes(cls, event)
+        
+        return None
+        
+        
+    
+    
--- api/views.py
+++ api/views.py
@@ -4,28 +4,6 @@ from .serializers import EventSerializer
 from .models import Note
 from .utils import read_agents
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        {'Endpoint': '/notes/',
-         'method': 'GET',
-         'body': None,
-         'description': 'Returns an array of notes'},
-        
-        {'Endpoint': '/notes/id',
-        'method': 'GET',
-        'body': None,
-        'description': 'Returns a single note object'},
-        
-        {'Endpoint': '/notes/create/',
-        'method': 'POST',
-        'body': {'body': ""},
-        'description': 'Creates new note with data sent in post req'},
-    ]
-    
-    
-    return Response(routes)
-
 
 @api_view(['POST'])
 def takeEvent(request):
@@ -42,6 +20,7 @@ def takeEvent(request):
             if result != None:
                 return Response(result, status=200)
             
-        return Response("None triggers was found", status=200)
+        return Response("None agents was found for this event type", status=404)
     
-    return Response(status=400)
+    return Response("Bad request", status=400)
+
