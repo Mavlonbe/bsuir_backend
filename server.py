ec8b3fb45ea7bd46062e3114feaa6dd8d3b7de61
V-outsider <voutsider@tutanota.com>
2023-12-15 05:52:20 +0300

    release

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-import uvicorn as uv
+import asyncio
+import uvicorn
 
-if __name__=='__main__':
-    uv.run('notes.asgi:application', host="0.0.0.0", port=8001)
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    config = uvicorn.Config("notes.asgi:application", host="0.0.0.0", port=23335)
+    server = uvicorn.Server(config)
+    loop.run_until_complete(server.serve())
